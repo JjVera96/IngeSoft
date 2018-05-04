f1539c55af2f276d1ab0b8fe34c9743a8ae08292
jjvera96 <jjvera96@gmail.com>
2018-05-04 11:07:34 -0500

    'Favicon'

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,5 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('app.urls')),
-    path('', views.index, name='super_index'),
+    path('', include('app.urls')),
 ]
